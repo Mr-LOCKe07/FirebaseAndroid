3cf15f7094cf13f9e3aab19c5dd63db2b2d7de3a
Mr-LOCKe07 <blsmutwiri@gmail.com>
2025-04-24 19:52:18 +0300

    Third Commit

--- app/src/main/java/com/blaise/firebase/ui/theme/screens/login/LoginScreen.kt
+++ app/src/main/java/com/blaise/firebase/ui/theme/screens/login/LoginScreen.kt
@@ -1,14 +1,23 @@
 package com.blaise.firebase.ui.theme.screens.login
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +30,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,50 +46,80 @@ import com.blaise.firebase.navigation.ROUTE_REGISTER
 fun Login_Screen(navController: NavHostController) {
     var email by remember { mutableStateOf(TextFieldValue("")) }
     var pass by remember { mutableStateOf(TextFieldValue("")) }
-    var context= LocalContext.current
+    val context= LocalContext.current
     Column(modifier = Modifier
         .fillMaxSize()
-        .background(Color.Blue),
-        horizontalAlignment = Alignment.CenterHorizontally) {
+        .background(Color.Black),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
 
         Text(text = "Login here",
-            color = Color.Cyan,
-            fontFamily = FontFamily.Cursive,
-            fontSize = 30.sp)
-        Spacer(modifier = Modifier.height(20.dp))
+            color = Color.White,
+            fontFamily = FontFamily.Monospace,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold)
 
+        Spacer(modifier = Modifier.height(20.dp))
         OutlinedTextField(value =email , onValueChange = {email=it},
-            label = { Text(text = "Enter Email") },
+            label = { Text(text = "Enter Email",
+                color = Color.White,
+                fontFamily = FontFamily.Serif,
+                fontSize = 20.sp) },
+            shape = RoundedCornerShape(16.dp),
+            leadingIcon = {
+                Icon(Icons.Default.Email,
+                contentDescription = "email")
+            },
             keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp),
 
             )
+        
         Spacer(modifier = Modifier.height(20.dp))
-
         OutlinedTextField(value =pass , onValueChange = {pass=it},
-            label = { Text(text = "Enter Password") },
+            label = { Text(text = "Enter Password",
+                color = Color.White,
+                fontFamily = FontFamily.Serif,
+                fontSize = 20.sp) },
+            shape = RoundedCornerShape(16.dp),
+            leadingIcon = {
+                Icon(Icons.Default.Lock,
+                    contentDescription = "password")},
             keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp)
         )
-        Spacer(modifier = Modifier.height(20.dp))
 
+        Spacer(modifier = Modifier.height(20.dp))
         Button(onClick = {
             val mylogin= AuthViewModel(navController, context )
-            mylogin.login(email.text.trim(),pass.text.trim())
-        }, modifier = Modifier.fillMaxWidth()) {
-            Text(text = "Log In")
+            mylogin.login(email.text.trim(),pass.text.trim())},
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(Color.Transparent),
+            border = BorderStroke(2.dp, Color.White)
+        ) {
+            Text(text = "Login",
+                color = Color.White,
+                fontFamily = FontFamily.Serif,
+                fontSize = 20.sp)
         }
-        Spacer(modifier = Modifier.height(20.dp))
 
-        Button(onClick = {
-            navController.navigate(ROUTE_REGISTER)
-        }, modifier = Modifier.fillMaxWidth()) {
-            Text(text = "Don't have account? Click to Register")
-        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(text = "Don't have account?",
+            color = Color.Red,
+            fontFamily = FontFamily.Serif,
+            fontSize = 20.sp)
+
+        Text(text = "Click to Register",
+            modifier = Modifier.clickable { ROUTE_REGISTER },
+            color = Color.White,
+            fontFamily = FontFamily.Serif,
+            fontSize = 25.sp,
+            fontStyle = FontStyle.Italic)
+        
 
     }
 }
--- app/src/main/java/com/blaise/firebase/ui/theme/screens/register/RegisterScreen.kt
+++ app/src/main/java/com/blaise/firebase/ui/theme/screens/register/RegisterScreen.kt
@@ -1,14 +1,24 @@
 package com.blaise.firebase.ui.theme.screens.register
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +31,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,6 +43,7 @@ import androidx.navigation.compose.rememberNavController
 import com.blaise.firebase.data.AuthViewModel
 import com.blaise.firebase.navigation.ROUTE_LOGIN
 
+
 @Composable
 fun Register_Screen(navController:NavHostController) {
     var email by remember { mutableStateOf(TextFieldValue("")) }
@@ -39,64 +52,90 @@ fun Register_Screen(navController:NavHostController) {
     val context= LocalContext.current
     Column(modifier = Modifier
         .fillMaxSize()
-        .background(Color.Cyan),
-        horizontalAlignment = Alignment.CenterHorizontally) {
+        .background(Color.Black),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
 
         Text(text = "Register here",
-            color = Color.Cyan,
-            fontFamily = FontFamily.Cursive,
+            color = Color.White,
+            fontFamily = FontFamily.Monospace,
             fontSize = 30.sp)
-        Spacer(modifier = Modifier.height(20.dp))
 
+        Spacer(modifier = Modifier.height(20.dp))
         OutlinedTextField(
             value = email, onValueChange = { email = it },
-            label = { Text(text = "Enter Email") },
-
+            label = { Text(text = "Enter Email",
+                color = Color.White,
+                fontFamily = FontFamily.Serif,
+                fontSize = 20.sp) },
+            shape = RoundedCornerShape(16.dp),
+            leadingIcon = { Icon(Icons.Default.Email,
+                contentDescription = "email") },
             keyboardOptions = KeyboardOptions . Default . copy (imeAction = ImeAction.Next),
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp),
 
             )
-        Spacer(modifier = Modifier.height(20.dp))
 
+        Spacer(modifier = Modifier.height(20.dp))
         OutlinedTextField(value =pass , onValueChange = {pass=it},
-            label = { Text(text = "Enter password") },
+            label = { Text(text = "Enter password",
+                color = Color.White,
+                fontFamily = FontFamily.Serif,
+                fontSize = 20.sp) },
+            shape = RoundedCornerShape(16.dp),
+            leadingIcon = {Icon(Icons.Default.Lock,
+                contentDescription = "passsword")},
             keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp)
         )
+
         Spacer(modifier = Modifier.height(20.dp))
         OutlinedTextField(value =confirmpass , onValueChange = {
             confirmpass=it},
-            label = { Text(text = "Enter Confirm Pass") },
-
+            label = { Text(text = "Enter Confirmation Pass",
+                color = Color.White,
+                fontFamily = FontFamily.Serif,
+                fontSize = 20.sp) },
+            shape = RoundedCornerShape(16.dp),
+            leadingIcon = {Icon(Icons.Default.CheckCircle,
+                contentDescription = "confirmpass")},
             keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp)
         )
+
         Spacer(modifier = Modifier.height(20.dp))
-
-
         Button (onClick = {
             val myregister= AuthViewModel(navController,context)
-            myregister.signup(email.text.trim(),pass.text.trim(),confirmpass.text.trim())
-
-
-
-
-        }, modifier = Modifier.fillMaxWidth()) {
-            Text(text = "Register ")
+            myregister.signup(email.text.trim(),pass.text.trim(),confirmpass.text.trim()) },
+            modifier = Modifier.fillMaxWidth(),
+            border = BorderStroke(2.dp, Color.White),
+            colors = ButtonDefaults.buttonColors(Color.Transparent)
+        ) {
+            Text(text = "Register ",
+                color = Color.White,
+                fontFamily = FontFamily.Serif,
+                fontSize = 20.sp)
         }
+
         Spacer(modifier = Modifier.height(20.dp))
+        Text(text = "Already have an account?",
+            color = Color.Red,
+            fontFamily = FontFamily.Serif,
+            fontSize = 20.sp)
 
-        Button(onClick = {
-            navController.navigate(ROUTE_LOGIN)
-        }, modifier = Modifier.fillMaxWidth()) {
-            Text(text = "Have an Account? Click to Login")
-        }
+        Text(text = "Click to Login",
+            modifier = Modifier.clickable { ROUTE_LOGIN },
+            color = Color.White,
+            fontFamily = FontFamily.Serif,
+            fontSize = 25.sp,
+            fontStyle = FontStyle.Italic)
+
 
     }
 
